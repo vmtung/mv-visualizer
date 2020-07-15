dd9f70fa8a2185efd85f75372319d3bbf5a4b290
Tung <vmtung93@gmail.com>
2020-07-15 11:22:46 +0700

    finalize visual, fade title in/out when start music

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AudioSpectrum from 'react-audio-spectrum'
 import logo from './logo.svg';
+import {Animated} from "react-animated-css";
 
 import './App.css';
 
-const onInputChange = (e) => {
+const onAudioInputChange = (e) => {
   var sound = document.getElementById('sound');
   sound.src = URL.createObjectURL(e.target.files[0]);
+}
+const onVideoInputChange = (e) => {
+  var video = document.getElementById('video');
+  video.src = URL.createObjectURL(e.target.files[0]);
+  video.loop
 }
 
 const getLocalSettings = () => ({
@@ -50,20 +56,73 @@ const App = () => {
     const [meterColor1, setMeterColor1] = useState(defaultSettings.meterColor1)
     const [meterColor2, setMeterColor2] = useState(defaultSettings.meterColor2)
     const [meterColor3, setMeterColor3] = useState(defaultSettings.meterColor3)
+
+    const [audioInput, setAudioInput] = useState('')
+    const [audioFiles, setAudioFiles] = useState([])
+    const [currentAudioIndex, setCurrentAudioIndex] = useState(-1)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [showTitle, setShowTitle] = useState(false)
+    const [titles, setTitles] = useState([])
+    const [artists, setArtists] = useState([])
+    const [titleColors, setTitleColors] = useState([])
+
+    useEffect(() => {
+      var video = document.getElementById('video');
+      var sound = document.getElementById('sound');
+      if (isPlaying) {
+        sound.play()
+        video.play()
+        setShowTitle(true)
+      } else {
+        sound.pause();
+        sound.src = sound.src;
+        video.pause()
+        video.src = video.src
+      }
+    }, [isPlaying])
+    useEffect(() => {
+      var sound = document.getElementById('sound');
+      if (audioFiles[currentAudioIndex]) {
+        sound.src = URL.createObjectURL(audioFiles[currentAudioIndex]);
+        sound.onended = () => {
+          console.log('ended')
+          setShowTitle(false)
+          if (audioFiles.length > currentAudioIndex + 1) {
+            setTimeout(() => {
+              setCurrentAudioIndex(currentAudioIndex + 1)
+              setTimeout(() => {
+                sound.play()
+                setShowTitle(true)
+              }, 1000)
+            }, 2000)
+          }
+        }
+      }
+    }, [currentAudioIndex])
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Audio Visualizer</h1>
-        </header>
+        </header> */}
         <div style={{
           display: 'flex',
         }}>
           <div className="setting-section file">
-            <input type="file" id="input" accept="audio/*" onChange={onInputChange} />
-            <audio id="sound" controls></audio>
+            <h3>Music & Video files</h3>
+            <label>Music</label>
+            <input multiple value={audioInput} type="file" id="input1" accept="audio/*" onChange={(e) => {
+              setAudioInput(e.target.value)
+              setAudioFiles(e.target.files)
+              setCurrentAudioIndex(0)
+            }} />
+            <label>Video</label>
+            <input type="file" id="input2" accept="video/*" onChange={onVideoInputChange} />
+            <button onClick={() => setTimeout(() => setIsPlaying(true), 2000)} >Play</button>
+            <audio id="sound"></audio>
           </div>
           <div className="setting-section props">
+            <h3>Visualizer</h3>
             <PropInput
               label="Cap Color"
               value={capColor}
@@ -112,8 +171,7 @@ const App = () => {
               onChange={e => setMeterColor3(e.target.value)}
               type="text"
             />
-          </div>
-          <div className="setting-section save">
+
             <button onClick={() => setLocalSettings({
               capColor,
               capHeight,
@@ -125,26 +183,97 @@ const App = () => {
               meterColor3,
             })}>Save settings</button>
           </div>
+          
+          <div className="setting-section">
+            <h3>Song Title</h3>
+            {
+              Array.prototype.map.call(audioFiles, function(file, index) {
+                return <div className="setting-section props">
+                  <label>{file.name}</label>
+                    <PropInput
+                      label="Title"
+                      value={titles[index]}
+                      onChange={e => {
+                        titles[index] = e.target.value
+                        setTitles(titles)
+                      }}
+                      type="text"
+                    />
+                    <PropInput
+                      label="Artist"
+                      value={artists[index]}
+                      onChange={e => {
+                        artists[index] = e.target.value
+                        setArtist(artists)
+                      }}
+                      type="text"
+                    />
+                    <PropInput
+                      label="Title Color"
+                      value={titleColors[index]}
+                      onChange={e => {
+                        titleColors[index] = e.target.value
+                        setTitleColors(titleColors)
+                      }}
+                      type="text"
+                    />
+                </div>
+              })
+            }
+          </div>
         </div>
-        <AudioSpectrum
-          id="audio-canvas"
-          style={{
-            padding: 10
-          }}
-          height={500}
-          width={900}
-          audioId={'sound'}
-          capColor={capColor}
-          capHeight={capHeight}
-          meterWidth={meterWidth}
-          meterCount={meterCount}
-          meterColor={[
-            {stop: 0, color: meterColor1},
-            {stop: 0.5, color: meterColor2},
-            {stop: 1, color: meterColor3}
-          ]}
-          gap={gap}
-        />
+        <div style={{
+          position: 'absolute',
+          top: 0,
+          padding: 5,
+          display: !isPlaying ? 'none' : 'block'
+        }}>
+          <video muted loop style={{
+            width: '100%',
+          }} id="video"></video>
+          <Animated animationInDelay={1000} animationInDuration={2000} animationOutDuration={2000} animationIn="fadeIn" animationOut="fadeOut" isVisible={showTitle}>
+           <div className="fade-in-title" style={{
+              color: titleColors[currentAudioIndex] || 'white',
+            }}>{titles[currentAudioIndex] || ''}</div>
+          </Animated>
+          <Animated animationInDelay={1000} animationInDuration={2000} animationOutDuration={2000} animationIn="fadeIn" animationOut="fadeOut" isVisible={showTitle}>
+            <div className="fade-in-title" style={{
+              bottom: '6vw',
+              color: titleColors[currentAudioIndex] || 'white',
+              fontSize: '2vw',
+            }}>{artists[currentAudioIndex] || ''}</div>
+          </Animated>
+          <div className="spectrum-cont">
+            <AudioSpectrum
+              id="audio-canvas"
+              style={{
+                paddingBottom: '13vw',
+                paddingTop: '5vw',
+                width: '80%',
+              }}
+              height={500}
+              width={900}
+              audioId={'sound'}
+              capColor={capColor}
+              capHeight={capHeight}
+              meterWidth={meterWidth}
+              meterCount={meterCount}
+              meterColor={[
+                {stop: 0, color: meterColor1},
+                {stop: 0.5, color: meterColor2},
+                {stop: 1, color: meterColor3}
+              ]}
+              gap={gap}
+            />
+          </div>
+          <button style={{
+            position: 'absolute',
+            left: 0,
+            bottom: -10,
+          }} onClick={() => setIsPlaying(false)}>
+            Close
+          </button>
+        </div>
       </div>
     );
 }
